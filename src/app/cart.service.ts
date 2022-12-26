60554772df85ef015fe5cee8bbb9b741e7ef7384
Matt2991 <112526429+Matt2991@users.noreply.github.com>
2022-12-25 22:02:03 -0600

    new cache items in git ignore and syntax changed to fix shipping error

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,16 +1,14 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Product } from './products';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Product } from "./products";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CartService {
   items: Product[] = [];
-  
-  constructor(
-    private http: HttpClient
-  ) { }
+
+  constructor(private http: HttpClient) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -26,6 +24,8 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get<{type: string, price: number}[]>('/assets/shippinh.json');
+    return this.http.get<{ type: string; price: number }[]>(
+      "/assets/shipping.json"
+    );
   }
 }
